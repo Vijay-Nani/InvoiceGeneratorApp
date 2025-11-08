142cee024b312669fe2b59adef48f967d47a63b3
Vijay-Nani <vijaygone1123@gmail.com>
2025-11-08 14:48:01 +0530

    Implemented Signup screen with fields

---
+++ app/src/main/java/vijay/project/invoicegenerator/SignupActivity.kt
@@ -0,0 +1,13 @@
+package vijay.project.invoicegenerator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
